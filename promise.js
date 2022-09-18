d0cb84420d9474e00b1e3b957cbf53cd0e0127fc
Ivana Asus Gatsu <ivana-helvin@gmail.com>
2022-09-18 15:57:22 +0000

    ivana ekasanthi

--- promise.js
+++ promise.js
@@ -1,7 +1,30 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(ekspresi) => {
+	//let marah = 0;
+	//let gak = 0;
+	let hitung = 0;
+
+	try {
+		const xxi = await promiseTheaterIXX();
+		const cgv = await promiseTheaterVGC();
+
+		const gabung = xxi.concat(cgv);
+
+		const counts = {}
+		for (eks of gabung){
+			//return(eks['hasil']);
+			if(eks['hasil']==ekspresi){
+				hitung = hitung + 1;
+			}
+		}
+
+		return hitung;
+	} catch (err) {
+		console.log(err);
+	}
+};
 
 module.exports = {
   promiseOutput,
